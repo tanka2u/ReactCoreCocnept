feecbfae716e293f6922584a572e322d8a1a02fd
tanka2u <ptanka123@gmail.com>
2024-08-23 20:04:45 +0200

    React basic

---
+++ src/components/Todos.tsx
@@ -0,0 +1,24 @@
+import React, {useContext} from "react";
+
+import TodoItem from "./TodoItem";
+import { TodosContext } from "../store/todos-context";
+import classes from "./Todos.module.css";
+
+const Todos: React.FC = (
+  
+) => {
+    const todoCtx = useContext(TodosContext)
+  return (
+    <ul className={classes.todos}>
+      {todoCtx.items.map((item) => (
+        <TodoItem
+          key={item.id}
+          text={item.text}
+          onRemoveTodo={todoCtx.removeTodo.bind(null, item.id)}
+        />
+      ))}
+    </ul>
+  );
+};
+
+export default Todos;
